662586a6fb0779ca3c436a82b3216f832c6e9a71
tomoya shibazaki <tshibazaki8165@gmail.com>
2021-07-12 23:13:28 +0900

    add lambda apigateway

---
+++ src/handlers/test_func.py
@@ -0,0 +1,7 @@
+def handler(event, context):
+    return {
+        'isBase64Encoded': False,
+        'statusCode': 200,
+        'headers': {},
+        'body': '{"message": "Hello from AWS Lambda"}'
+    }
